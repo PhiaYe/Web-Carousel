b2a8fd2d8e26413778b2d7336e50bd4d15ad1bd8
PhiaYe <sophye005@gmail.com>
2018-02-12 23:39:56 -0500

    I hope this works

--- js/script.js
+++ js/script.js
@@ -1,27 +1,150 @@
-var slideInterval = 3500;
 
-startPlayback();
 
-function getFigures() {
-    return document.getElementById('carousel').getElementsByTagName('figure');
+var c;
+var ctx;
+var x = 150;
+var y = 100;
+var shape;
+
+window.onload = function () {
+  c=document.getElementById("canvas");
+  ctx=c.getContext("2d");
+};
+
+
+
+function changeColor(newColor) {
+  var elem = document.getElementById('para');
+  elem.style.color = newColor;
 }
 
-function nextImg(){
-    var curPic;
-    var figures = getFigures();
-    for(var i = 0; i < figures.length; i ++){
-            if(figures[i].className == 'visible'){
-                figures[i].className = ''
-                curPic = i + 1;
-            }
-        }    
-    if (curPic == figures.length) {
-        curPic = 0;
-    }
-    figures[curPic].className = 'visible';
-    setTimeout(nextImg, slideInterval);
-}   
+//-----------[class Circle starts here]------------------//
 
-function startPlayback() {    
-    setTimeout(nextImg, slideInterval);
+class Circle{
+
+//  var x = 150;
+//  var y = 100
+
+  constructor(x, y) {
+      this.r = 50;
+      this.start = 0;
+      this.end = 2*Math.PI;
+      this.x = 150;
+      this.y = 100;
+      this.strokeStyle = "#000000"
+      this.lineWidth = 5;
+      this.removeLineWidth = 7;
+      this.drawCircle(this.x, this.y);
+  }
+
+  drawCircle(x,y){
+    ctx.beginPath();
+    ctx.arc(x, y, this.r, this.start, this.end);
+    ctx.strokeStyle = this.strokeStyle;
+    ctx.lineWidth = this.lineWidth;
+    ctx.stroke();
+  }
+
+  removeCircle(x,y){
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.r, this.start, this.end);
+    ctx.strokeStyle="#FFFFFF";
+    ctx.lineWidth = this.removeLineWidth;
+    ctx.stroke();
+  }
+
+  moveUp(x,y){
+    this.removeCircle(this.x, this.y)
+    y = this.y - 10;
+    ctx.beginPath();
+    ctx.arc(x,y,50,0,2*Math.PI);
+    ctx.strokeStyle="#000000";
+    ctx.lineWidth=5;
+    ctx.stroke();
+  }
+
+  moveDown(x,y){
+
+  }
+
+  moveLeft(x,y){
+
+  }
+
+  moveRight(x,y){
+
+  }
+
+
 }
+
+//-----------[class Circle ends here]------------------//
+
+
+
+document.addEventListener('keydown', function(event) {
+
+      switch (event.keyCode) {
+
+        case 80: //p
+          shape = new Circle(x,y);
+        break;
+
+        case 87: //w
+          shape = shape.moveUp(x,y);
+         break;
+
+        case 65: //a
+          ctx.beginPath();
+          ctx.arc(x,y,50,0,2*Math.PI);
+          ctx.strokeStyle="#FFFFFF";
+          ctx.lineWidth=10;
+          ctx.stroke();
+
+          x = x - 10;
+
+          ctx.beginPath();
+          ctx.arc(x,y,50,0,2*Math.PI);
+          ctx.strokeStyle="#000000";
+          ctx.lineWidth=5;
+          ctx.stroke();
+         break;
+
+         case 68: //d
+           ctx.beginPath();
+           ctx.arc(x,y,50,0,2*Math.PI);
+           ctx.strokeStyle="#FFFFFF";
+           ctx.lineWidth=10;
+           ctx.stroke();
+
+           x = x + 10;
+
+           ctx.beginPath();
+           ctx.arc(x,y,50,0,2*Math.PI);
+           ctx.strokeStyle="#000000";
+           ctx.lineWidth=5;
+           ctx.stroke();
+          break;
+
+          case 83: //s
+             ctx.beginPath();
+             ctx.arc(x,y,50,0,2*Math.PI);
+             ctx.strokeStyle="#FFFFFF";
+             ctx.lineWidth=10;
+             ctx.stroke();
+
+             y = y + 10;
+
+             ctx.beginPath();
+             ctx.arc(x,y,50,0,2*Math.PI);
+             ctx.strokeStyle="#000000";
+             ctx.lineWidth=5;
+             ctx.stroke();
+            break;
+
+
+
+        default:
+          break;
+      }
+  });
